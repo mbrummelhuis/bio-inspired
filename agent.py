512d12266c1552bd1cf5cde094f218134bc5451b
mbrummelhuis <mbrummelhuis@gmail.com>
2021-07-02 17:01:21 +0200

    Laatste vrijdag commit

--- agent.py
+++ agent.py
@@ -1,13 +1,14 @@
 class Agent():
     def __init__(self,env):
         self.name = 'Continuous agent'
-    
-    def getRandomAction(self, env):
+        self.action_space = env.action_space
+
+    def getRandomAction(self):
         # Sample a random action from the action space
-        action = env.action_space.sample()
+        action = self.action_space.sample()
         return action
     
-    def getAction(self,state):
+    def getAction(self,state,policy):
         # Sample an action based on the policy and state
-
+        
         return action
--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import gym
 from agent import Agent
 
 env_name = "LunarLanderContinuous-v2" # Set environment
-number_episodes = 200 # Sets number of episodes
+number_episodes = 10 # Sets number of episodes
 
 # Create environment specified earlier and reset
 env = gym.make(env_name)
 env.reset()
-
-# Set variables to initial state
-done = False
-total_reward = 0
 
 # Initialise agent
 agent = Agent(env)
 
 # Main running loops, outer loop is episodes and inner loop is frames in each episode
 for episode in range(number_episodes):
+    # Set variables to initial episode state
+    print("Episode ", episode)
+    env.reset()
+    done = False
+    total_reward = 0
     while done == False:
-        action = agent.getRandomAction(env)
-        print('Action: ', action)
-        state, reward, done, info = env.step(env.action_space.sample())
+        action = agent.getRandomAction()
+        state, reward, done, info = env.step(action)
         env.render()
         total_reward += reward
 
-print("Total reward: ", total_reward)
+    print("Total reward: ", total_reward)
+
 env.close()
 
