af1868196804f2c7397efd317cf70130eb28340e
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2021-07-23 15:56:21 +0200

    EOS push
    Tutorial afgemaakt en bugs gefixt voor de discrete versie

--- agent.py
+++ agent.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.functional as F
 import numpy as np
+from network import Network
 
 class Agent():
-    def __init__(self, env, gamma, epsilon, lr, batch_size, input_dims, hidden_dims, output_dims, max_mem_size=100000, eps_end=0.01, eps_dec=5e-4):
-        self.name = 'Continuous agent'
-        self.action_space = env.action_space
+    def __init__(self, gamma, epsilon, lr, batch_size, input_dims, hidden_dims, output_dims, max_mem_size=100000, eps_end=0.01, eps_dec=5e-5):
+        self.action_space = [i for i in range(output_dims)]
 
         self.gamma = gamma
         self.epsilon = epsilon
@@ -21,13 +21,13 @@ class Agent():
         self.Q_eval = Network(self.lr, input_dims = input_dims, hidden_dims = hidden_dims, output_dims = output_dims)
 
         self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, input_dims), dtype = np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype = np.float32)
 
         self.action_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype = np.bool)
 
-    def store_transition(self, state, action, reward, state_, done): #state_ is new state
+    def storeTransition(self, state, action, reward, state_, done): #state_ is new state
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         self.new_state_memory[index] = state_
@@ -54,3 +54,33 @@ class Agent():
         return action
 
     def learn(self):
+
+        if self.mem_cntr < self.batch_size:
+            return
+        
+        self.Q_eval.optimizer.zero_grad() # Set gradient of optimizer to zero
+
+        max_mem = min(self.mem_cntr, self.mem_size)
+        batch = np.random.choice(max_mem, self.batch_size, replace=False)
+
+        batch_index = np.arange(self.batch_size, dtype = np.int32)
+
+        # Bring batches of all variables to GPU
+        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
+        new_state_batch = T.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
+        reward_batch = T.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
+        terminal_batch = T.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
+
+        action_batch = self.action_memory[batch]
+
+        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
+        q_next = self.Q_eval.forward(new_state_batch) # Use target network here when you want to improve
+        q_next[terminal_batch] = 0.0
+        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
+
+        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
+        loss.backward()
+        self.Q_eval.optimizer.step()
+
+        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min \
+            else self.eps_min
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import gym
 from agent import Agent
+import numpy as np
 
-def main():
-    env_name = "LunarLanderContinuous-v2" # Set environment
-    number_episodes = 10 # Sets number of episodes
-
+def main(verbose = True):
+    env_name = "LunarLander-v2" # Set environment
+    number_episodes = 250 # Sets number of episodes
+    scores, eps_history = [],[]
     # Create environment specified earlier and reset
     env = gym.make(env_name)
     env.reset()
 
     # Initialise agent
-    agent = Agent(env)
+    agent = Agent(0.99, 1.0, 0.005, 64, [8], 64, 4)
 
     # Main running loops, outer loop is episodes and inner loop is frames in each episode
     for episode in range(number_episodes):
         # Set variables to initial episode state
-        print("Episode ", episode)
-        env.reset()
         done = False
-        total_reward = 0
-        while done == False:
-            action = agent.getRandomAction()
-            state, reward, done, info = env.step(action)
-            env.render()
-            total_reward += reward
+        ep_reward = 0
+        observation = env.reset()
 
-        print("Total reward: ", total_reward)
+        while done == False:
+            action = agent.getAction(observation)
+            observation_, reward, done, info = env.step(action)
+            ep_reward += reward
+            agent.storeTransition(observation, action, reward, observation_, done)
+            agent.learn()
+            observation = observation_
+        scores.append(ep_reward)
+        eps_history.append(agent.epsilon)
+
+        avg_score = np.mean(scores[-100:])
+        if verbose:
+            print("Episode ", episode, "Episode reward: ", ep_reward, "Epsilon ", agent.epsilon)
 
     env.close()
     return
 
 if __name__ == '__main__':
-    main()
+    main(True) # Set true for verbose
 
