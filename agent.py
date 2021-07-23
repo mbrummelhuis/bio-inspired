33acb5c1dabee4eb792c4bb709386ca4a273f96f
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2021-07-23 14:02:11 +0200

    Progression
    Progression on coding the assignment

--- agent.py
+++ agent.py
@@ -1,7 +1,44 @@
+import torch as T
+import torch.nn as nn
+import torch.optim as optim
+
+class Network(nn.module):
+    def __init__(self, lr, input_dims, hidden_dims, output_dims, activation):
+        super(Network, self).__init__()
+
+        self.input_dims = input_dims
+        self.hidden_dims = hidden_dims
+        self.output_dims = output_dims
+        self.activation = activation
+
+        self.fc1 = nn.Linear(self.input_dims, self.hidden_dims)
+        self.fc2 = nn.Linear(self.hidden_dims, self.output_dims)
+        self.optimizer = optim.Adam(self.parameters, lr=lr)
+        self.loss = nn.MSELoss()
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu') # Set compute device to CUDA GPU if available, use CPU otherwise
+        self.to(self.device)
+
+    def forward(self, state): # Maybe use if statement to enable using different activation functions? (ReLU, tanh, RBF, sigmoid)
+        x = self.
+
+
 class Agent():
-    def __init__(self,env):
+    def __init__(self, env, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=100000, eps_end=0.01, eps_dec=5e-4):
         self.name = 'Continuous agent'
         self.action_space = env.action_space
+
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
+        self.lr = lr
+        self.mem_size = max_mem_size
+        self.batch_size = batch_size
+        self.mem_cntr = 0
+
+        self.Q_eval = 
+
 
     def getRandomAction(self):
         # Sample a random action from the action space
--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import gym
 from agent import Agent
 
-env_name = "LunarLanderContinuous-v2" # Set environment
-number_episodes = 10 # Sets number of episodes
 
-# Create environment specified earlier and reset
-env = gym.make(env_name)
-env.reset()
+def main():
+    env_name = "LunarLanderContinuous-v2" # Set environment
+    number_episodes = 10 # Sets number of episodes
 
-# Initialise agent
-agent = Agent(env)
-
-# Main running loops, outer loop is episodes and inner loop is frames in each episode
-for episode in range(number_episodes):
-    # Set variables to initial episode state
-    print("Episode ", episode)
+    # Create environment specified earlier and reset
+    env = gym.make(env_name)
     env.reset()
-    done = False
-    total_reward = 0
-    while done == False:
-        action = agent.getRandomAction()
-        state, reward, done, info = env.step(action)
-        env.render()
-        total_reward += reward
 
-    print("Total reward: ", total_reward)
+    # Initialise agent
+    agent = Agent(env)
 
-env.close()
+    # Main running loops, outer loop is episodes and inner loop is frames in each episode
+    for episode in range(number_episodes):
+        # Set variables to initial episode state
+        print("Episode ", episode)
+        env.reset()
+        done = False
+        total_reward = 0
+        while done == False:
+            action = agent.getRandomAction()
+            state, reward, done, info = env.step(action)
+            env.render()
+            total_reward += reward
+
+        print("Total reward: ", total_reward)
+
+    env.close()
+    return
+
+
+
+if __name__ == '__main__':
+    main()
 
