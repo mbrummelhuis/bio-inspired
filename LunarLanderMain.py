bb280fd0d72b65415476e820a929b0a9c3fa343f
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2021-08-11 10:38:51 +0200

    Code from Phil
    Added the code from Phil and bugfixed to get it to run on my computer. Planning to add some other features as well.

--- LunarLanderMain.py
+++ LunarLanderMain.py
@@ -1,12 +1,18 @@
-from ddpg import Agent
+from ddpg_phil import Agent
 import gym
 import numpy as np
+from utils import plotLearning
+from datetime import datetime
 
 env = gym.make('LunarLanderContinuous-v2')
 
 agent = Agent(alpha=0.00025, beta=0.00025, input_dims=[8], tau=0.001, env=env, batch_size=64, layer1_size=400, layer2_size=300, n_actions=2)
 
 np.random.seed(0)
+
+begin_time = datetime.now()
+time_checkpoints = []
+time_checkpoints.append(begin_time)
 
 score_history = []
 for i in range(1000):
@@ -23,7 +29,9 @@ for i in range(1000):
     
     score_history.append(score)
     print('Episode ', i, 'Score %.2f' % score, '100 game average %.2f' % np.mean(score_history[-100:]))
+
     if i % 25 == 0:
         agent.save_models()
     
     filename = 'lunar-lander.png'
+    plotLearning(score_history, filename, window=100)
