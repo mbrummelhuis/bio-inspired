2a00dcbff1c316bcd11cb978ec59babfd3069418
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2021-08-11 16:17:42 +0200

    Cleanup
    Cleaned up all the redundant files and changed a bit here and there. Overall progress.

--- LunarLanderMain.py
+++ LunarLanderMain.py
@@ -1,25 +1,34 @@
-from ddpg_phil import Agent
+from ddpg import Agent
 import gym
 import numpy as np
-from utils import plotLearning
+from utils import plotLearning, displayTimeEstimate
 from datetime import datetime
+import json
 
 env = gym.make('LunarLanderContinuous-v2')
 
+# All these inputs should become hyperparameters too
 agent = Agent(alpha=0.00025, beta=0.00025, input_dims=[8], tau=0.001, env=env, batch_size=64, layer1_size=400, layer2_size=300, n_actions=2)
 
 np.random.seed(0)
 
-begin_time = datetime.now()
-time_checkpoints = []
-time_checkpoints.append(begin_time)
-
+time_checkpoints = [datetime.now()]
+total_episodes = 1000 # This becomes a hyperparameter
+episodes_interval= 50 # This becomes a hyperparameter
+visual = False # This becomes a hyperparameter
 score_history = []
-for i in range(1000):
+
+# Get hyperparameters here and display them before running.
+
+
+
+for episode in range(total_episodes):
     done = False
     score = 0
     obs = env.reset()
     while not done:
+        if visual:
+                env.render()
         act = agent.choose_action(obs)
         new_state, reward, done, info = env.step(act)
         agent.remember(obs, act, reward, new_state, int(done))
@@ -28,10 +37,14 @@ for i in range(1000):
         obs = new_state
     
     score_history.append(score)
-    print('Episode ', i, 'Score %.2f' % score, '100 game average %.2f' % np.mean(score_history[-100:]))
+    print('Episode ', episode, 'Score %.2f' % score, ', 100 game average %.2f' % np.mean(score_history[-100:]))
 
-    if i % 25 == 0:
+    if episode % episodes_interval == 0 and episode != 0:
         agent.save_models()
+        time_checkpoints = displayTimeEstimate(time_checkpoints,episodes_interval=episodes_interval,total_episodes=total_episodes)
     
     filename = 'lunar-lander.png'
     plotLearning(score_history, filename, window=100)
+
+print("Done!")
+print("Total time: ", datetime.now()-time_checkpoints[0])
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
 import numpy as np
+from datetime import datetime
 
 def plotLearning(scores, filename, x=None, window=5):   
     N = len(scores)
@@ -10,5 +11,23 @@ def plotLearning(scores, filename, x=None, window=5):
         x = [i for i in range(N)]
     plt.ylabel('Score')       
     plt.xlabel('Game')                     
-    plt.plot(x, running_avg)
+    plt.plot(x, running_avg,color='b')
+    plt.plot(x, scores,color='r')
     plt.savefig(filename)
+
+def displayTimeEstimate(time_list, episodes_interval=10, total_episodes=1000):
+    now = datetime.now()
+    time_elapsed = now - time_list[-1]
+    total_time_elapsed = now - time_list[0]
+
+    print("Time of last ", episodes_interval, " episodes: ", time_elapsed)
+    print("Elapsed time: ", total_time_elapsed)
+
+    episode_number = len(time_list) * episodes_interval
+    time_estimate = total_time_elapsed / episode_number * total_episodes
+    time_left = time_estimate - total_time_elapsed
+
+    print('Estimated time left: ', time_left)
+
+    time_list.append(now)
+    return time_list
